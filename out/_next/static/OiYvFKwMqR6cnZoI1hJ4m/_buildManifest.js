2ac06a5e5650f24d52594ce4848d6284c75c9841
Alvaro Padilla <varoboss.ap@gmail.com>
2023-04-25 22:30:52 -0600

    Deploy to Github Pages

---
+++ out/_next/static/OiYvFKwMqR6cnZoI1hJ4m/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8f9b7ab00cf234fc.js"],"/_error":["static/chunks/pages/_error-3986dd5834f581dc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
